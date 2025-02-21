6eff05b23d1966223edaabdbe355fde7d27645b1
debjitdey1 <debjitdey559@gmail.com>
2025-02-21 16:00:52 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all the js code in new version/ JavascriptVersion
+// alert(4+9); // we are using NodeJs , Not browser
+
+/*
+DataTypeS ?
+
+==>string(EX: hello world, Debjit)
+==>Integer(Ex: 54548,478)
+==> Float(Ex: 45.20,001.35)
+==>Charecter(Ex: 97 ACII values)
+==>boolean(Ex: True/False)
+*/
+// null is the standalone values
